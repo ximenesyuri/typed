4d698c7a0526fcdbd1479e66e0eb94959a983126
yxm <yxm.dev@gmx.ie>
2025-05-29 19:24:55 -0300

    feat: add Env class

--- typed/examples.py
+++ typed/examples.py
@@ -6,3 +6,5 @@ from typed.mods.helper_examples import _is_json_table, _is_json_flat
 JsonTable     = Filter(Json, typed(_is_json_table))
 JsonFlat      = Filter(Dict(Str, Any), typed(_is_json_flat))
 JsonFlatEntry = Regex(r'^[a-zA-Z0-9_.]+$')
+
+Env = Regex(r"^[A-Z0-9_]+$")
