61f0a68d2417415d63fdaf377981386c5c8b6e7a
yxm <yxm.dev@gmx.ie>
2025-05-29 10:26:47 -0300

    ...

--- typed/main.py
+++ typed/main.py
@@ -1,15 +1,13 @@
 from functools import wraps
-from typed.mods.types.builtin import *
-from typed.mods.types.func    import *
-from typed.mods.types.struc   import *
-from typed.mods.ops.generics  import *
-from typed.mods.ops.base      import *
-from typed.mods.ops.func      import *
+from typed.mods.types.builtin       import *
+from typed.mods.types.func          import *
+from typed.mods.types.struc         import *
+from typed.mods.factories.generics  import *
+from typed.mods.factories.base      import *
+from typed.mods.factories.func      import *
 
 def typed(func):
     """Decorator that wraps a function with the appropriate TypedFuncType subclass."""
-    sig = inspect.signature(func)
-    return_annotation = sig.return_annotation
     wrapped_func = TypedFuncType(func)
     try:
         wrapped_func = BoolFuncType(func)
