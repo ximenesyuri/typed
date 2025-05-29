a1718175173866f43b16c4fa52531800058c06b3
yxm <yxm.dev@gmx.ie>
2025-05-29 11:38:48 -0300

    ...

--- typed/mods/types/base.py
+++ typed/mods/types/base.py
@@ -1,9 +1,10 @@
 from typed.mods.factories.base import Union, Dict, Set, List
 
-Int = int
-Str = str
-Bool = bool
+Int   = int
+Str   = str
+Bool  = bool
 Float = float
+Type  = type
 
 class Any(type):
     def __instancecheck__(cls, instance):
