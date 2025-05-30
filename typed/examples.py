2c368d64c19fb2521f7b4831cdbbbfa1407d5d2f
yxm <yxm.dev@gmx.ie>
2025-05-30 11:18:22 -0300

    feat: add RGB and HEX classes

--- typed/examples.py
+++ typed/examples.py
@@ -1,10 +1,16 @@
 from typed.main import typed
-from typed.mods.factories.generics import Filter, Regex
-from typed.mods.types.base import Json, Str, Any, Dict
-from typed.mods.helper_examples import _is_json_table, _is_json_flat
+from typed.mods.factories.base     import Prod
+from typed.mods.factories.generics import Filter, Regex, Range
+from typed.mods.types.base         import Json, Str, Any, Dict
+from typed.mods.helper_examples    import _is_json_table, _is_json_flat
 
+# Json
 JsonTable     = Filter(Json, typed(_is_json_table))
 JsonFlat      = Filter(Dict(Str, Any), typed(_is_json_flat))
 JsonFlatEntry = Regex(r'^[a-zA-Z0-9_.]+$')
 
 Env = Regex(r"^[A-Z0-9_]+$")
+
+# Color
+RGB = Prod(Range(0, 255), 3)
+HEX = Regex(r"^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$")
