6bdf1eb2148f07c5a172c55b37676823e34ca586
yxm <yxm.dev@gmx.ie>
2025-07-09 15:08:00 -0300

    fix:  minor fixin

--- typed/main.py
+++ typed/main.py
@@ -1,7 +1,8 @@
-from typed.mods.helper.helper import _nill, _get_null_object
+from typed.mods.helper.helper       import _nill, _get_null_object
 from typed.mods.types.base          import *
 from typed.mods.types.func          import *
 from typed.mods.types.attr          import *
+from typed.mods.types.other         import *
 from typed.mods.factories.generics  import *
 from typed.mods.factories.specifics import *
 from typed.mods.factories.base      import *
