eb575aaee4a84199d611b9ea11cdcb41fd24d2bc
yxm <yxm.dev@gmx.ie>
2025-08-27 09:18:03 -0300

    feat: include PARAMETRIC metafactory

--- typed/mods/factories/meta.py
+++ typed/mods/factories/meta.py
@@ -5,6 +5,58 @@ from typed.mods.meta.base import _TYPE_
 from typed.mods.helper.helper import _name, _name_list
 
 @cache
+def PARAMETRIC(*types: Type, factory) -> Type:
+    from typed.mods.types.base import TYPE
+    from typed.mods.types.func import Factory
+
+    if not factory:
+        raise ValueError(
+            "Missing value in 'PARAMETRIC' factory\n"
+            f" ==> 'factory': required argument were not set."
+        )
+
+    if not types:
+        return factory
+
+    if not isinstance(factory, Factory):
+        raise TypeError(
+            "Wrong type in PARAMETRIC factory: \n"
+            f" ==> {_name(factory)}: has unexpected type\n"
+            f"     [expected_type] a subtype of Factory"
+            f"     [received_type] {_name(type(factory))}"
+        )
+
+    for typ in types:
+        if not isinstance(typ, TYPE):
+            raise TypeError(
+                "Wrong type in PARAMETRIC factory: \n"
+                f" ==> {_name(typ)}: has unexpected type\n"
+                f"     [expected_type] a subtype of TYPE"
+                f"     [received_type] {_name(type(typ))}"
+            )
+
+    TYPES = (TYPE(typ) for typ in types)
+    class _PARAMETRIC_(*TYPES):
+        def __instancecheck__(cls, instance):
+            return all(isinstance(instance, typ) for typ in types)
+        def __subclasscheck__(cls, subclass):
+            return all(issubclass(subclass, typ) for typ in types)
+        def __call__(self, *args, **kwargs):
+            if not args and not kwargs:
+                return self._type()
+            elif args and isinstance(args[0], type):
+                return self._factory(*args, **kwargs)
+            else:
+                return self._type(*args, **kwargs)
+
+    class_name = _name(base_type)
+    return _PARAMETRIC_(class_name, (base_type,), {
+        "__display__": class_name,
+        "base_type": base_type,
+        "factory": factory
+    })
+
+@cache
 def SUBTYPES(*types: Tuple_[Type]) -> Type:
     """
     Build the metatype of subtypes of a given types.
@@ -12,8 +64,11 @@ def SUBTYPES(*types: Tuple_[Type]) -> Type:
         > is a type T such that issubclass(T, K) is True
         > for some K in (X, Y, ...)
     """
+    if not types:
+        from typed.mods.types.base import Nill
+        return Nill
     for typ in types:
-        if not issubclass(type(typ), TYPE):
+        if not isinstance(typ, TYPE):
             raise TypeError(
                 "Wrong type in SUBTYPES metafactory: \n"
                 f" ==> {_name(typ)}: has unexpected type\n"
@@ -42,7 +97,7 @@ def NOT(*types: Tuple_[Type]) -> Type:
         > T is not X, Y, ...
     """
     for typ in types:
-        if not issubclass(type(typ), TYPE):
+        if not isinstance(typ, TYPE):
             raise TypeError(
                 "Wrong type in NOT metafactory: \n"
                 f" ==> {_name(typ)}: has unexpected type\n"
