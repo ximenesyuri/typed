3b816ede58a927b4226fc2f84743a3dbb983fdb9
yxm <yxm.dev@gmx.ie>
2025-09-01 16:59:58 -0300

    fix: minor fixing

--- typed/factories.py
+++ typed/factories.py
@@ -2,7 +2,8 @@ from typed.mods.factories.base import (
     Union, Prod, UProd
 )
 from typed.mods.factories.generics import (
-    Inter, Filter, Compl, Regex, Range, Not, Enum, Single, Len
+    Type, Free,
+    Inter, Filter, Compl, Regex, Range, Not, Enum, Single, Null, Len, Maybe
 )
 from typed.mods.factories.meta import (
     SUBTYPES, NOT, PARAMETRIC
