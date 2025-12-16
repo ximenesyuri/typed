a77c732bb40539c879aba29c0c0a3eabed25bd9f
yxm <yxm.dev@gmx.ie>
2025-12-16 19:22:25 -0300

    fix: minor fixing

--- typed/mods/types/extra.py
+++ typed/mods/types/extra.py
@@ -39,7 +39,7 @@ UUID.__display__ = "UUID"
 _DATETIME_DIRECTIVES = r"(%[YmdHMSfIMjUwWaAbBcxXzZpI])"
 _DATE_DIRECTIVES = r"(%[YmdjUwWaAbBcxXzZ])"
 _TIME_DIRECTIVES = r"(%[HMSfIZp])"
-_ALLOWED_CHARS = r"(\s|[^%])*"
+_ALLOWED_CHARS = r"([/\-:\s]|[^%])*"
 
 DatetimeFormat = Regex(f"^{_ALLOWED_CHARS}({_DATETIME_DIRECTIVES}{_ALLOWED_CHARS})+$")
 DateFormat = Regex(f"^{_ALLOWED_CHARS}({_DATE_DIRECTIVES}{_ALLOWED_CHARS})+$")
